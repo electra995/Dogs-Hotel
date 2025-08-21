0a3c502bb850f212411e2b578469a11c9a944870
elect <electra995@msn.com>
2025-08-21 04:05:54 +0200

    aggiunto l'url di railway

--- Frontend/src/config/config.jsx
+++ Frontend/src/config/config.jsx
@@ -1,3 +1,4 @@
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "dogs-hotel-production.up.railway.app";
+// const API_BASE_URL = "http://localhost:8000/api";
 
 export {API_BASE_URL};
